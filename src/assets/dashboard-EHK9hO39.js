1e9904d3f60b32d1a23885d8eef7f56f95c962c9
harvenzhang <73051262+harvenzhang@users.noreply.github.com>
2025-08-09 16:11:34 +0800

    update src file from pakeplus

---
+++ src/assets/dashboard-EHK9hO39.js
@@ -0,0 +1,1 @@
+import{a,c as e,d as r}from"./index-wHxvx3OI.js";import o from"./dashboard-B-1N8GQ1.js";const _={__name:"dashboard",setup(t){return(c,s)=>(a(),e("div",null,[r(o)]))}};export{_ as default};
